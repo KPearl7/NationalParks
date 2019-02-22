9a15dded6d36a277fb35d2963f52a8d3bc16d4e8
KathrynXandreya <43448877+KPearl7@users.noreply.github.com>
2019-02-21 19:54:19 -0700

    final drill

--- index.js
+++ index.js
@@ -5,33 +5,34 @@ const BASE_URL = "http://api.nps.gov/api/v1/parks"
 const API ="cWi11y9RDx0AsAVxM5WfivxwaJiAwhJ3801wyEpU"
 
 function createQueryString(params) {
- const paramKeys = Object.keys(params)
- const encodedParams = paramKeys.map(key => {
- const encodedKey = encodeURIComponent(key)
- const encodedValue = encodeURIComponent(params[key])
- return `${encodedKey}=${encodedValue}`
- })
- return encodedParams.join('&');
+    const paramKeys = Object.keys(params)
+    const encodedParams = paramKeys.map(key => {
+        const encodedKey = encodeURIComponent(key)
+        const encodedValue = encodeURIComponent(params[key])
+        return `${encodedKey}=${encodedValue}`
+    });
+    return encodedParams.join('&');
 }
 
 
-function showResults(ressults, state)  {
+function showResults(results, state)  {
     console.log(results);
     $('#parks').empty();
 
-    let parks = results.data
+    let parks = results.data;
     if (parks === undefined || parks.length == 0) {
-        $('#parks').append(`<p>No info for this state ${state}</p>`)
+        $('#parks').append(`<p>State does not exist ${state}</p>`)
     }  else {
  
-    for(let i = 0; i < responseJson.length; i++) {
-        let parksReturned = responseJson[i];
-        ('#parks').append (`<li><h3>"${parksReturned.fullName}"></h3></li>
-        <p>"${parksReturned.description}"</p>
-        <a href="${parksReturned.url}">Click here for Website</a>`)
+        for(let i = 0; i < parks.length; i++) {
+            let parksReturned = parks[i];
+            $('#parks').append(`<li><h3>"${parksReturned.fullName}"></h3></li>
+                <p>"${parksReturned.description}"</p>
+                <a href="${parksReturned.url}">Click here for Website</a>`)
         }
         $('.hidden').show();
-};
+    }
+}
 
 
 function getParks(state, maxResults=25) {
@@ -44,23 +45,25 @@ function getParks(state, maxResults=25) {
     const url = BASE_URL + '?' + queryString;
     console.log(url);
     
-fetch(url) 
-.then(response => (response.json())
-.then(responseJson => showResults(responseJson, state))
-.catch(err => { 
-    $('#js-error').text(`Parks not found`)
-})
-)};
+    fetch(url) 
+        .then(response => response.json())
+        .then(responseJson => showResults(responseJson, state))
+        .catch(err => { 
+            $('#js-error').text(`Parks not found`)
+        })
+}
 
 function clickSearch() {
-    $('#button').click(event => {
+    $('#js-form').submit(event => {
         event.preventDefault();
-    const state = $('#js-state-name').val();
+       
+        const state = $('#js-state-name').val();
+        
 
-    getParks(state);
-    console.log("getting park names now");
+        getParks(state);
+        console.log("getting park names now");
 
     });
 }
 
-(clickSearch);
+$(clickSearch);
